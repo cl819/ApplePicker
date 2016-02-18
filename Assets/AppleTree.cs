e831f47626ddf0e0708935c4bf81adc49f2e5b68
Courtney Lam <cl819@scarletmail.rutgers.edu>
2016-02-17 21:50:19 -0500

    Moving Tree
    Basic movement up to Changing Direction

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -2,14 +2,32 @@
 using System.Collections;
 
 public class AppleTree : MonoBehaviour {
+	public GameObject applePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops =1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		//changing direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //move right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); //move left
+		}
 	
 	}
+
+
+
 }
